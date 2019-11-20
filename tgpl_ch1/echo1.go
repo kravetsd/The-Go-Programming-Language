474a391e31ab2c2aca4b80c40ef4d53d4004c427
Kravets, Dmitriy <Dmitriy.Kravets@wolterskluwer.com>
2019-11-20 23:16:23 +0300

    added some comments

--- tgpl_ch1/echo1.go
+++ tgpl_ch1/echo1.go
@@ -4,8 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"strings"
-
-	test_folder_module "github.com/oth_packs"
+	test_folder_module "tgpl-github/oth_packs"
 )
 
 var n = flag.Bool("n", false, "ommit trailing newline")
@@ -22,6 +21,8 @@ func main() {
 }
 
 func Hello() string {
+	//comment here
+	//There is a comment just to fix syntax of the golang
 	s := "Hello wotld!"
 	return s
 
